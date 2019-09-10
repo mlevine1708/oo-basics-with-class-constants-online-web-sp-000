59268a4cd76785dedb0380964e22ad1e382004fa
Micah Levine <micah.levine@outlook.com>
2019-09-10 01:49:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition, :brand
-  attr_reader
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-  BRANDS = []
+  BRANDS = [ ]
 
   def initialize(brand)
     @brand = brand
